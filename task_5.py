99b9e47117c675068649a9ef4e180a0475d22a32
Kuzurmann <kuzurman@inbox.ru>
2022-04-24 20:13:22 +0300

    initial commit

---
+++ task_5.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+
+def db_connect(query):
+    connection = sqlite3.connect('netflix.db')
+    cursor = connection.cursor()
+    cursor.execute(query)
+    result = cursor.fetchall()
+    connection.close()
+    return result
+
+def get_actors(name1='Rose McIver', name2='Ben Lamb'):
+    query = f"""
+            SELECT
+                "cast"
+            FROM netflix
+            WHERE "cast" LIKE '%{name1}%'
+                AND "cast" LIKE '%{name2}%'
+        """
+    response = db_connect(query)
+    actors = []
+    for cast in response:
+        actors.extend(cast[0].split(', '))
+    result = []
+    for a in actors:
+        if a not in [name1, name2]:
+            if actors.count(a) > 2:
+                result.append(a)
+
+    result = set(result)
+    print(result)
+get_actors()
---
+++ task_6.py
@@ -0,0 +1,33 @@
+import sqlite3
+
+def db_connect(query):
+    connection = sqlite3.connect('netflix.db')
+    cursor = connection.cursor()
+    cursor.execute(query)
+    result = cursor.fetchall()
+    connection.close()
+    return result
+
+def get_films(type_film, release_year, genre):
+    query = f"""
+            SELECT
+                title
+                , description
+                , "type"
+            FROM netflix
+            WHERE "type" = '{type_film}'
+                AND release_year = {release_year}
+                AND listed_in LIKE '%{genre}%'
+        """
+    response = db_connect(query)
+    response_json = []
+    for film in response:
+        response_json.append({
+            'title': film[0],
+            'description': film[1],
+            'type': film[2],
+        })
+    return response_json
+
+
+print(get_films(type_film='Movie', release_year=2016, genre='Dramas'))
